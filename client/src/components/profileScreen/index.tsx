464c3858df69e4ba6e1333e57d6d60072de8560d
renanmdreis <renanmdreis@alu.ufc.br>
2023-07-21 23:58:50 -0300

    feat - add profile screen

---
+++ client/src/components/profileScreen/index.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
+import { useUpdateUserMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+
+
+export const ProfileScreen = () => {
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <Form.Container>
+            
+        </Form.Container>
+    )
+}
